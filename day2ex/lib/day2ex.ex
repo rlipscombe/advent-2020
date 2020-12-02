93a01da158ff176d00c0f11dacddf4b88f512843
Roger Lipscombe <roger@differentpla.net>
2020-12-02 10:49:03 +0000

    Day 2: Elixir, part 2

--- day2ex/lib/day2ex.ex
+++ day2ex/lib/day2ex.ex
@@ -14,17 +14,27 @@ defmodule Day2ex do
         [String.to_integer(min), String.to_integer(max), ch, password]
       end)
 
-    input
-    |> Enum.count(fn [min, max, ch, password] ->
-      count =
-        password
-        |> String.graphemes()
-        |> Enum.count(fn
-          ^ch -> true
-          _ -> false
-        end)
+    input |> Enum.count(&new_rules/1) |> IO.inspect()
+  end
 
-      min <= count && count <= max
-    end) |> IO.inspect
+  defp old_rules([min, max, ch, password]) do
+    count =
+      password
+      |> String.graphemes()
+      |> Enum.count(fn
+        ^ch -> true
+        _ -> false
+      end)
+
+    min <= count && count <= max
+  end
+
+  defp new_rules([a, b, ch, password]) do
+    case {String.at(password, a - 1), String.at(password, b - 1)} do
+      {^ch, ^ch} -> false
+      {^ch, _} -> true
+      {_, ^ch} -> true
+      {_, _} -> false
+    end
   end
 end
