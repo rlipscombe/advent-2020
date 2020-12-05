7e7c51f3c8692c7f62940b4b4dff71daeddfa56c
Roger Lipscombe <roger@differentpla.net>
2020-12-05 15:39:17 +0000

    Day 3, part 1: Rust

--- day3rs/src/main.rs
+++ day3rs/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    let values: Vec<Vec<u8>> = std::fs::read_to_string("input")
+        .expect("input file")
+        .lines()
+        .map(|x| x.as_bytes().to_vec())
+        .collect();
+
+    let mut trees = 0;
+
+    let mut x = 0;
+    let mut y = 0;
+
+    while y < values.len() {
+        // '.' = 46 = space
+        // '#' = 35 = tree
+        let curr = values[y][x];
+        //println!("{}", curr);
+        if curr == 35 {
+            trees += 1;
+        }
+        x += 3;
+        x %= values[y].len();
+        y += 1;
+    }
+
+    println!("{}", trees);
 }
