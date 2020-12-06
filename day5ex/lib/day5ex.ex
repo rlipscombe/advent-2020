372c04da42329581cf20e21c1d0a54ab4d73a630
Roger Lipscombe <roger@differentpla.net>
2020-12-06 21:27:20 +0000

    Day 5: Elixir

---
+++ day5ex/lib/day5ex.ex
@@ -0,0 +1,52 @@
+defmodule Day5ex do
+  def main(_args) do
+    seats =
+      File.read!("input")
+      |> String.split("\n", trim: true)
+      |> Enum.map(&convert/1)
+
+    _part1 =
+      seats
+      |> Enum.max()
+      |> IO.inspect()
+
+    # Note: throw as non-local return
+    _part2 =
+      try do
+        seats
+        |> Enum.sort()
+        |> Enum.reduce(fn
+          x, acc when x == acc + 1 -> x
+          _x, acc -> throw(acc + 1)
+        end)
+      catch
+        x -> x
+      end
+      |> IO.inspect()
+
+    # Alternatively: do it by explicitly generating the pairs
+    [{part2, _} | _] =
+      seats |> Enum.sort() |> pairs() |> Enum.drop_while(fn {x, y} -> x + 1 == y end)
+
+    (part2 + 1) |> IO.inspect()
+  end
+
+  defp pairs(enumerable) do
+    next_pairs(enumerable, []) |> Enum.reverse()
+  end
+
+  defp next_pairs([a, b | rest] = _enumerable, init) do
+    next_pairs([b | rest], [{a, b} | init])
+  end
+
+  defp next_pairs(_enumerable, init), do: init
+
+  defp convert(s) do
+    s
+    |> String.replace("F", "0")
+    |> String.replace("B", "1")
+    |> String.replace("L", "0")
+    |> String.replace("R", "1")
+    |> String.to_integer(2)
+  end
+end
