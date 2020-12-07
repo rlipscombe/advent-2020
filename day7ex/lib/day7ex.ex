de81881aff43b66dfb82dea4c2d2c2f89cd91ab5
Roger Lipscombe <roger@differentpla.net>
2020-12-07 10:23:09 +0000

    sdhdshds

--- day7ex/lib/day7ex.ex
+++ day7ex/lib/day7ex.ex
@@ -1,17 +1,40 @@
 defmodule Day7ex do
   def main(_args) do
+    name = "input"
+
     rules =
-      File.read!("input")
+      File.read!(name)
       |> String.split("\n", trim: true)
       |> Enum.map(&parse_rule/1)
+
+    # Write that to a .png, so we can inspect it.
+    #rules |> to_dot() |> dot_to_png(name)
+
+    # Convert the rules to a digraph.
+    g = Graph.new(type: :directed)
+
+    g =
+      Enum.reduce(rules, g, fn {outer, inners}, g ->
+        Enum.reduce(inners, g, fn {count, inner}, g ->
+          Graph.add_edge(g, outer, inner, weight: count)
+        end)
+      end)
+
+    #Graph.to_dot(g) |> dot_to_png(name <> "g")
+
+    # Then search for our bag colour. From there, how many roots are there?
+    options = Graph.reaching(g, ["shiny gold"])
+
+    length(options) - 1 |> IO.inspect
   end
 
   defp parse_rule(s) do
     [outer, inners] = Regex.run(~r/^(.*) bags contain (.*).$/, s, capture: :all_but_first)
-    {outer, parse_inners(inners)} |> IO.inspect
+    {outer, parse_inners(inners)}
   end
 
   defp parse_inners("no other bags"), do: []
+
   defp parse_inners(s) do
     s |> String.split(", ") |> Enum.map(&parse_inner/1)
   end
@@ -19,5 +42,30 @@ defmodule Day7ex do
   defp parse_inner(s) do
     [count, colour] = Regex.run(~r/(\d+) (.*) bags?/, s, capture: :all_but_first)
     {String.to_integer(count), colour}
+  end
+
+  defp to_dot(rules) do
+    assigns = [rules: rules]
+
+    dot =
+      EEx.eval_string(
+        """
+        digraph G {
+          <%= for {outer, inners} <- rules do %>
+            <%= for {_, inner} <- inners do %>
+              "<%= outer %>" -> "<%= inner %>";
+            <% end %>
+          <% end %>
+        }
+        """,
+        assigns
+      )
+
+    {:ok, dot}
+  end
+
+  defp dot_to_png({:ok, dot}, name) do
+    File.write!(name <> ".dot", dot)
+    {_, 0} = System.cmd("dot", ["-Tpng", "-o", "#{name}.png", "#{name}.dot"])
   end
 end
--- day7ex/mix.exs
+++ day7ex/mix.exs
@@ -10,15 +10,20 @@ defmodule Day7ex.MixProject do
       escript: escript_options()
     ]
   end
-  
+
+  def application do
+    [extra_applications: [:eex]]
+  end
+
   defp escript_options do
     [
       main_module: Day7ex
     ]
   end
-  
+
   defp deps do
     [
+      {:libgraph, "~> 0.7"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
