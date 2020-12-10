d80188ec4d43cf803364ba8df75385dd0af001d7
Roger Lipscombe <roger@differentpla.net>
2020-12-10 08:26:01 +0000

    Day 10, Elixir, Part 1

--- day10ex/lib/day10ex.ex
+++ day10ex/lib/day10ex.ex
@@ -1,5 +1,19 @@
 defmodule Day10ex do
-  def main(_args) do
-    IO.puts("Hello World!")
+  def main([path]) do
+    values = File.read!(path) |> String.split() |> Enum.map(&String.to_integer/1)
+
+    {ones, threes} =
+      values
+      |> Enum.sort()
+      |> Enum.chunk_every(2, 1, :discard)
+      |> Enum.reduce({1, 1}, fn [a, b], {ones, threes} ->
+        case b - a do
+          1 -> {ones + 1, threes}
+          3 -> {ones, threes + 1}
+        end
+      end)
+
+    product = ones * threes
+    IO.puts("part 1 = #{product}")
   end
 end
