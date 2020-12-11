71e6b7deb2b387c1477a7a388f32a50051c47e00
Roger Lipscombe <roger@differentpla.net>
2020-12-11 11:17:56 +0000

    Load and render initial state

--- day11rs/src/main.rs
+++ day11rs/src/main.rs
@@ -1,3 +1,49 @@
+enum Cell {
+    Floor,
+    Empty,
+    Occupied,
+}
+
+type Grid = Vec<Vec<Cell>>;
+
 fn main() {
-    println!("Hello, world!");
+    //let path = "input";
+    let path = "example";
+    let grid: Vec<Vec<Cell>> = std::fs::read_to_string(path)
+        .expect("input file")
+        .lines()
+        .map(parse_line)
+        .collect();
+
+    // Does Rust have ncurses support, or am I just going to clear the screen each time?
+    render(grid);
+}
+
+fn parse_line(line: &str) -> Vec<Cell> {
+    line.chars()
+        .map(|c| match c {
+            'L' => Cell::Empty,
+            '.' => Cell::Floor,
+            '#' => Cell::Occupied,
+            _ => panic!()
+        })
+        .collect()
+}
+
+fn render(grid: Grid) {
+    print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+    for r in grid {
+        for c in r {
+            render_cell(c);
+        }
+        println!();
+    }
+}
+
+fn render_cell(cell: Cell) {
+    match cell {
+        Cell::Empty => { print!("L"); }
+        Cell::Floor => { print!("."); }
+        Cell::Occupied => { print!("#"); }
+    }
 }
