093e005dba55592f79c471f50af52cb7a667641e
Roger Lipscombe <roger@differentpla.net>
2020-12-15 15:31:20 +0000

    dhgshsh

--- day15ex/lib/day15ex.ex
+++ day15ex/lib/day15ex.ex
@@ -1,7 +1,10 @@
 defmodule Day15ex do
   def main(_args) do
-    numbers = [0, 3, 6]
-    turns = 2020 #10
+    # numbers = [0, 3, 6]
+    numbers = [1, 20, 11, 6, 12, 0]
+    # turns = 10
+    # turns = 2020
+    turns = 30_000_000
 
     map =
       numbers
@@ -17,25 +20,36 @@ defmodule Day15ex do
   end
 
   defp next_turn(turn, turns, last, map) when turn <= turns do
-    #IO.puts("Turn #{turn}: last number was #{last}")
+    # IO.puts("Turn #{turn}: last number was #{last}")
 
-    next = case Map.get(map, last) do
-      [t] when t == turn - 1 ->
-        #IO.puts("not seen before")
-        0
-      [t] ->
-        #IO.puts("last seen #{t}")
-        (turn - 1) - t
-      [a, b | _] ->
-        #IO.puts("last seen #{a} and #{b}")
-        a - b
-    end
+    next =
+      case Map.get(map, last) do
+        [t] when t == turn - 1 ->
+          # IO.puts("not seen before")
+          0
 
-    IO.puts("Turn #{turn}: #{next}")
+        [t] ->
+          # IO.puts("last seen #{t}")
+          turn - 1 - t
 
-    map = Map.update(map, next, [turn], fn ts -> [turn | ts] end)
+        [a, b | _] ->
+          # IO.puts("last seen #{a} and #{b}")
+          a - b
+      end
+
+    # case rem(turn, 10_000) do
+    #   0 -> IO.puts("Turn #{turn}: #{next}")
+    #   _ -> nil
+    # end
+
+    map =
+      Map.update(map, next, [turn], fn
+        [t] -> [turn, t]
+        [t0, _t1] -> [turn, t0]
+      end)
+
     next_turn(turn + 1, turns, next, map)
   end
 
-  defp next_turn(_, _, _, _), do: nil
+  defp next_turn(_, turn, last, _), do: IO.puts("Turn #{turn}: #{last}")
 end
